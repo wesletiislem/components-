ffcea3a36ecc1727df28503d50939ef6ec7c7cab
wesletiislem <esleem250@gmail.com>
2022-02-20 14:16:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './pdp.jpg';
+
+function App() {
+  return (
+    <div >
+      <h1 class="title red">Eslem</h1>
+      <h1 class="title red">Tunis</h1>
+      <img src={img}/>
+      <img src="./Pro.jpg" />
+       
+    </div>
+  );
+}
+
+export default App;
